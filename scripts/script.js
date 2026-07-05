fdfdd21b228cde91ad3da6bec216a6043fd9293c
Muneer-S-A-05 <61188210+Muneer-S-A-05@users.noreply.github.com>
2026-07-05 17:24:32 +0530

    Gave functionality to Title Bar buttons
    - Made red and yellow button minimize the main window
    - A home button will show up when its minimized
    - Got rid of the green button as i dont have a functionality for it yet
    - Minimizing and maximizing has smooth distorted animation
    - Next up i guess is making a folder window
    - Need to decide on a design for that window

--- scripts/script.js
+++ scripts/script.js
@@ -16,25 +16,59 @@ function showTime() {
     document.getElementById('date').textContent = dateStr.slice(0,3)+dateStr.slice(4);
 }
 
+function minimizeMainWindow(){
+    mainWindow.animate([
+        {transform : 'translate(0px,0px) scale(1,1) rotateX(0deg)'} ,
+        {transform : `translate(0px,10%) scale(0.3,0.5) rotateX(-25deg)`},
+        {transform : `translate(0px,80%) scale(0,0) rotateX(-45deg)`}
+    ],{
+        duration : closeSpeed,
+        easing : 'ease-out',
+        // animate changes are usually temporary, fill changes that
+        fill : 'forwards'
+    });
+    setTimeout(()=>{
+        mainWindowMaximizeButton.style.display = 'flex'
+    },closeSpeed)
+}
+
+function maximizeMainWindow(){
+    mainWindowMaximizeButton.style.display = 'none'
+    mainWindow.animate([
+        {transform : `translate(0px,${mainWindowHeight}px) scale(0,0) rotateX(-45deg)`},
+        {transform : `translate(0px,${mainWindowHeight/10}px) scale(0.3,0.6) rotateX(-25deg)`},
+        {transform : 'translate(0px,0px) scale(1,1) rotateX(0deg)'}
+    ],{
+        duration : closeSpeed,
+        easing : 'ease-out',
+        fill : 'forwards'
+    });
+}
+
 // Time and Date in titlebar
 showTime();
 setInterval(showTime, 1000);
 
-const titlbarGragArea = document.querySelector(".dragarea");
+// MAIN WINDOW OBJECT
 const mainWindow = document.querySelector(".mainWindow");
+
+// MKAING MAIN WINDOW DRAGGABLE
+const titlbarGragArea = document.querySelector(".dragarea");
 
 let isDraggable = false;
 let offsetX = 0;
 let offsetY = 0;
 
-titlbarGragArea.addEventListener("mousedown", (event) => {
+// draggin starts
+titlbarGragArea.addEventListener("pointerdown", (event) => {
     isDraggable = true;
     titlbarGragArea.style.cursor = "grabbing";
     offsetX = event.clientX - mainWindow.offsetLeft;
     offsetY = event.clientY - mainWindow.offsetTop;
 })
 
-window.addEventListener("mousemove", (event) => {
+// tracking mouse movements
+window.addEventListener("pointermove", (event) => {
     if (!isDraggable) return;
     let newX = event.clientX - offsetX;
     let newY = event.clientY - offsetY;
@@ -42,9 +76,25 @@ window.addEventListener("mousemove", (event) => {
     mainWindow.style.top = `${newY}px`;
 })
 
-titlbarGragArea.addEventListener("mouseup", (event) => {
+// dragging ends
+titlbarGragArea.addEventListener("pointerup", (event) => {
     if (isDraggable){
         isDraggable = false;
         titlbarGragArea.style.cursor = "grab";
     }
 })
+
+// CLOSING THE MAIN WINDOW
+const closeSpeed = 400;
+const mainWindowMinimizeButton = document.querySelectorAll(".butt");
+const mainWindowMaximizeButton = document.querySelector(".mainWindowButton");
+const mainWindowHeight = mainWindow.offsetHeight;
+
+// giving it perspective to cause distortion
+mainWindow.parentElement.style.perspective = "1000px";
+
+// Minimizing Main Window
+mainWindowMinimizeButton.forEach(button => button.addEventListener("click",minimizeMainWindow));
+
+// Reopening main window
+mainWindowMaximizeButton.addEventListener('click',maximizeMainWindow);
