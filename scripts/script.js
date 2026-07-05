c1fdf131013922dd1bcc6b9d6101875df3f13aae
Muneer-S-A-05 <61188210+Muneer-S-A-05@users.noreply.github.com>
2026-07-05 12:09:36 +0530

    Made main window draggable
    - Now the main window can be dragged
    - Dragging can only be done using titlebar
    - Removed the title text from the there
    - Kinda loosing the point of calling it titlebar

--- scripts/script.js
+++ scripts/script.js
@@ -12,11 +12,39 @@ function showTime() {
         month: 'short'
     });
 
-    // Puts them into the HTML
     document.getElementById('time').textContent = timeStr;
     document.getElementById('date').textContent = dateStr.slice(0,3)+dateStr.slice(4);
 }
 
-// Start immediately, then update every second
+// Time and Date in titlebar
 showTime();
 setInterval(showTime, 1000);
+
+const titlbarGragArea = document.querySelector(".dragarea");
+const mainWindow = document.querySelector(".mainWindow");
+
+let isDraggable = false;
+let offsetX = 0;
+let offsetY = 0;
+
+titlbarGragArea.addEventListener("mousedown", (event) => {
+    isDraggable = true;
+    titlbarGragArea.style.cursor = "grabbing";
+    offsetX = event.clientX - mainWindow.offsetLeft;
+    offsetY = event.clientY - mainWindow.offsetTop;
+})
+
+window.addEventListener("mousemove", (event) => {
+    if (!isDraggable) return;
+    let newX = event.clientX - offsetX;
+    let newY = event.clientY - offsetY;
+    mainWindow.style.left = `${newX}px`;
+    mainWindow.style.top = `${newY}px`;
+})
+
+titlbarGragArea.addEventListener("mouseup", (event) => {
+    if (isDraggable){
+        isDraggable = false;
+        titlbarGragArea.style.cursor = "grab";
+    }
+})
